53e9da2d46917224c14338d983d4bc09398c0fd2
Khaw335 <kunlasatree_s@cmu.ac.th>
2021-06-05 09:24:39 +0700

    commit

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById('ok').addEventListener('click',function(e)
+{
+    alert('ok')
+    console.log(e)
+})
+document.getElementById('cancle').addEventListener('click',function(e)
+{
+    alert{'cancle'}
+    console.loglog(e)
+})
+document.getElementById('no').addEventListener('click',function(e)
+{
+    alert{'no'}
+    console.loglog(e)
+})
+
+document.getElementById('cancle'.addEventListener('mousmove',function(e)
+{
+    var x = c.client;
+    var y = c.client;
+    console.log('x;'+x+','+'y:'+y)
+})
+document.getElementById('ok'.addEventListener('mousmove',function(e)
+{
+    var x = c.client;
+    var y = c.client;
+    console.log('x;'+x+','+'y:'+y)
+})
+document.getElementById('no'.addEventListener('mousmove',function(e)
+{
+    var x = c.client;
+    var y = c.client;
+    console.log('x;'+x+','+'y:'+y)
+})
